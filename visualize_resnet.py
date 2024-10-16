3c0577e31254eedc34bbc7b7eedbe6bf6e44e9eb
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-10-16 09:30:13 -0600

    TEMP(ANCHORS-Visualizer) : Fixes

--- visualize_resnet.py
+++ visualize_resnet.py
@@ -12,7 +12,7 @@ transform = transforms.Compose([transforms.Resize((640, 320)),
                                  transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                         std=[0.229, 0.224, 0.225])])
 
-img = Image.open('model/1.jpg')
+img = Image.open('model/inv.jpg')
 img_tensor = transform(img).unsqueeze(0)
 
 feature_volume = laneatt.forward(img_tensor).squeeze().permute(1, 2, 0).detach().numpy()
